45165bf61fbdfe5ea02c783935a23b0b2344385f
dnjsndnjs <37894963+dnjsndnjs@users.noreply.github.com>
2022-07-25 23:13:56 +0900

    implement MyPage

--- src/components/MainPageComponents/MyPage.js
+++ src/components/MainPageComponents/MyPage.js
@@ -1,7 +1,76 @@
+import React from "react";
+
 function MyPage() {
+    const [userId, setId] = React.useState('user id');
+    const [userName, setName] = React.useState('user name');
+    const [userPw, setPw] = React.useState('user password');
+    const [userGoal, setGoal] = React.useState('user goal');
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(userId);
+        console.log(userName);
+        console.log(userPw);
+        console.log(userGoal);
+    }
+
     return (
         <div>
-            <p>MyPage</p>
+            <h3>My Page</h3>
+            <form onSubmit={handleSubmit}>
+                <table>
+                    <tbody>
+                        <tr>
+                            <th>아이디</th>
+                            <td>
+                                <input
+                                    type="text"
+                                    name="userId"
+                                    value={userId}
+                                    onChange={(e) => setId(e.target.value)}
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>이름</th>
+                            <td>
+                                <input
+                                    type="text"
+                                    name="userName"
+                                    value={userName}
+                                    onChange={(e) => setName(e.target.value)}
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>비밀번호</th>
+                            <td>
+                                <input
+                                    type="password"
+                                    name="userPw"
+                                    value={userPw}
+                                    onChange={(e) => setPw(e.target.value)}
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>목표</th>
+                            <td>
+                                <input
+                                    type="text"
+                                    name="userGoal"
+                                    value={userGoal}
+                                    onChange={(e) => setGoal(e.target.value)}
+                                />
+                            </td>
+                        </tr>
+                        <tr>
+                            <th />
+                            <td><button style={{ float: 'right' }} type="submit">수정</button></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </form>
         </div>
     );
 };
--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -3,12 +3,10 @@ import React from "react";
 function MenuBar({ selectedMenu, onSelect }) {
     return (
         <div className='menu-bar'>
-            <ul>
-                <li onClick={() => onSelect(1)}>menu1</li>
-                <li onClick={() => onSelect(2)}>menu2</li>
-                <li onClick={() => onSelect(3)}>menu3</li>
-                <li onClick={() => onSelect(4)}>menu4</li>
-            </ul>
+            <p onClick={() => onSelect(1)} style={selectedMenu === 1 ? { backgroundColor: 'red' } : {}} >이번 주의 달성률</p>
+            <p onClick={() => onSelect(2)} style={selectedMenu === 2 ? { backgroundColor: 'red' } : {}}>이번 달의 달성률</p>
+            <p onClick={() => onSelect(3)} style={selectedMenu === 3 ? { backgroundColor: 'red' } : {}}>각자의 목표</p>
+            <p onClick={() => onSelect(4)} style={selectedMenu === 4 ? { backgroundColor: 'red' } : {}}>오늘의 운동</p>
         </div>
     );
 };
