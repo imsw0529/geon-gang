31b7c9b9a86ca152c231572f16626a36f2060d87
dnjsndnjs <37894963+dnjsndnjs@users.noreply.github.com>
2022-07-25 22:04:21 +0900

    implement login hover

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-import Login from './components/Login';
 import User from './components/User';
 import MenuBar from './components/MenuBar';
 import MainPage from './components/MainPage';
--- src/components/User.js
+++ src/components/User.js
@@ -1,10 +1,17 @@
+import React from 'react';
 import people_icon from '../asset/people-icon.svg';
+import Login from './Login';
 
 function User() {
+    const [isLogin, setIsLogin] = React.useState(false);
+
     return (
-        <div style={{ display: 'flex', margin: '2vmin' }}>
-            <p style={{ marginRight: '1vmin' }}>user name</p>
-            <img src={people_icon} className='people-icon' />
+        <div className='user'>
+            <div style={{ display: 'flex' }}>
+                {isLogin ? <p style={{ marginRight: '1vmin' }}>user name</p> : null}
+                <img src={people_icon} className='people-icon' />
+            </div>
+            {isLogin ? null : <Login />}
         </div>
     );
 };
